d169a40dbdda0373151b326f0f6b97ca0bfa4474
umarchoudhary <m.umarc57@gmail.com>
2023-04-10 13:20:03 +0530

    first commit

---
+++ src/main/java/net/javaguide/springBoot/SpringbootBackendsApplication.java
@@ -0,0 +1,12 @@
+package net.javaguide.springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendsApplication.class, args);
+	}
+}
